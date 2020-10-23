86f7af74f84d39ebd4c3b72e74d9c064443afb3f
Felipe Alves Barboza <50283187+felipeab03@users.noreply.github.com>
2020-10-23 18:41:02 -0300

    feat: streamers by language

---
+++ API/test.py
@@ -0,0 +1,46 @@
+from twitchAPI import TwitchAPI
+import csv
+# 
+streamers=["alanzoka", "Cellbit", "loud_coringa", "XANDAOOGOD", "Gaules", "NOBRU", "loud_bak", "YoDa", "loud_thurzin", "loud_babi", "Baiano", "brtt", "jukes", "forever", "Smurfdomuca", "gafallen", "ziGueira", "adolfz", "fer", "Rakin", "aXtLOL", "deercheerup", "SkipNhO", "jovirone", "calango", "jean_mago", "Felps", "tinows", "RatoBorrachudo", "keiozin", "Hayashii", "casimito", "hastad", "boltz", "loud_voltan", "pijack11"]
+controllerAPI = TwitchAPI()
+
+def printResponse(query):
+  response = controllerAPI.get_response(query)
+  responseJSON = controllerAPI.printResponse(response)
+  return responseJSON
+#query = controllerAPI.get_user_streams_query(user_login)
+
+#query = controllerAPI.get_user_query(user_login)
+
+#query = controllerAPI.get_sub_count(38244180)
+
+
+with open('data2.csv', 'w', newline='') as file:
+  writer = csv.writer(file)
+  writer.writerow(["Streamer", "Date", "Duration", "Viewers"])
+  for streamer in streamers:
+    print(streamer)
+    i=0
+    streamerQuery = controllerAPI.get_user_streams_query(streamer)
+    streamerQueryJSON = printResponse(streamerQuery)
+    try:
+      streamer_id=streamerQueryJSON["data"][0]["user_id"]
+    except:
+      print(streamerQueryJSON)
+      continue
+    print(streamer_id)
+    cursor=''
+    while(i<6):
+      print(cursor)
+      query = controllerAPI.get_user_videos_query(streamer_id, cursor) 
+      jsonQuery = printResponse(query)
+      #print(jsonQuery)
+      try:
+        cursor=jsonQuery["pagination"]["cursor"]
+        for resp in jsonQuery["data"]:
+          writer.writerow([resp["user_name"], resp["created_at"], resp["duration"], resp["view_count"]])
+      except:
+        print(jsonQuery)
+      i=i+1
+      
+
--- webscrapforstremers.py
+++ webscrapforstremers.py
@@ -5,60 +5,86 @@ import csv
 streamerslink = []
 streamersname = []
 
+
+linguagens = ['russian']
+
+# for lingua in linguagens:
+#   with open(lingua + ".txt", 'r') as file:
+#     data = file.read()
+#     streamers = [line.strip() for line in data.split('\n') if line]
+
+
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
 driver = webdriver.Chrome(options=options)
 
+#https://twitchtracker.com/channels/rating/portuguese
+for lingua in linguagens:
+  with open(lingua+".txt", "w", encoding="utf-8") as f:
+    iterable = 1
+    while(iterable<=100):
+      print(iterable)
+      if(iterable==1):
+        driver.get('https://twitchtracker.com/channels/rating/'+lingua)
+      else:
+        driver.get('https://twitchtracker.com/channels/rating/'+lingua+'?page='+str(iterable))
+      try:
+        for k in range(2, 12):
+          streamerlink=driver.find_element_by_xpath("//*[@id='content-wrapper']/div[2]/div/div[1]/div["+str(k)+"]/div[3]/a").get_attribute('href')
+          streamername=driver.find_element_by_xpath("//*[@id='content-wrapper']/div[2]/div/div[1]/div["+str(k)+"]/div[3]/a").text
+          f.write(streamername + "," + streamerlink + '\n')
+      except:
+        iterable = iterable+1
+        continue
+      iterable=iterable+1  
+    f.close()      
 
-iterable = 1
-while(iterable<=100):
-  if(iterable==1):
-    driver.get('https://twitchtracker.com/channels/rating')
-  else:
-    driver.get('https://twitchtracker.com/channels/rating?page='+str(iterable))
-  for k in range(2, 12):
-    streamerslink.append(driver.find_element_by_xpath("//*[@id='content-wrapper']/div[2]/div/div[1]/div["+str(k)+"]/div[3]/a").get_attribute('href'))
-    streamersname.append(driver.find_element_by_xpath("//*[@id='content-wrapper']/div[2]/div/div[1]/div["+str(k)+"]/div[3]/a").text)
-  iterable=iterable+1        
+# print(streamersname, streamerslink)                     
 
-print(streamersname, streamerslink)                     
-
-with open('data.csv', 'w', newline='') as file:
-  writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=';')
-  writer.writerow(["Streamer", "Date", "Duration", "Viewers"])
-  for streamerlink in streamerslink:
-    streamername = streamersname[streamerslink.index(streamerlink)]
-    print(streamername)
-    try:
-      driver.get(streamerlink+'/streams')
-      dataControle = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr[1]/td[1]/a/span").text
-      pagina=1
-      ano = dataControle[dataControle.find(',')+2:dataControle.find(',')+6]
-      while (int(ano)>2018):
-        try:
-          dataBase = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr[1]/td[1]/a/span").text
-          if(pagina<=5):
-            driver.find_element_by_xpath("//*[@id='streams_paginate']/ul/li["+str(pagina)+"]/a").click()
-          else:
-            if(driver.find_element_by_xpath("//*[@id='streams_paginate']/ul/li[6]/a").text=='6'):
-              driver.find_element_by_xpath("//*[@id='streams_paginate']/ul/li[6]/a").click()
-            else:
-              driver.find_element_by_xpath("//*[@id='streams_paginate']/ul/li[5]/a").click()
-          if(pagina>2):
-            if(dataBase==driver.find_element_by_xpath("//*[@id='streams']/tbody/tr[1]/td[1]/a/span").text):
-              break
-          for i in range(1,21):
-            data = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr["+str(i)+"]/td[1]/a/span").text
-            data = data.replace('"', '')
-            duracao = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr["+str(i)+"]/td[2]/span").text
-            viewers = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr["+str(i)+"]/td[3]/span").text
-            viewers = viewers.replace('"', '')
-            viewers = viewers.replace(',', '.')
-            writer.writerow([streamername, data, duracao, viewers])
-            ano= data[data.find(',')+2:data.find(',')+6]
-          pagina+=1
-        except:
-          break
-    except:
-      continue  
+# with open('data.csv', 'w', newline='') as file:
+#   writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=';')
+#   writer.writerow(["Streamer", "Date", "Duration", "Viewers", "Language"])
+#   for streamerlink in streamerslink:
+#     streamername = streamersname[streamerslink.index(streamerlink)]
+#     print(streamername)
+#     try:
+#       driver.get(streamerlink)
+#       language = driver.find_element_by_xpath("//*[@id='content-wrapper']/div[8]/section[3]/div[1]/ul/li[5]/div[1]/div[2]/span").text
+#       print(language)
+#       //*[@id="content-wrapper"]/div[7]/section[3]/div[1]/ul/li[5]/div[1]/div[2]/span
+#       //*[@id="content-wrapper"]/div[8]/section[4]/div[1]/ul/li[5]/div[1]/div[2]/span
+#       driver.f
+#       driver.get(streamerlink+'/streams')
+#       dataControle = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr[1]/td[1]/a/span").text
+#       print(dataControle)
+#       pagina=1
+#       ano = dataControle[dataControle.find(',')+2:dataControle.find(',')+6]
+#       while (int(ano)>2018):
+#         try:
+#           dataBase = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr[1]/td[1]/a/span").text
+#           if(pagina<=5):
+#             driver.find_element_by_xpath("//*[@id='streams_paginate']/ul/li["+str(pagina)+"]/a").click()
+#           else:
+#             if(driver.find_element_by_xpath("//*[@id='streams_paginate']/ul/li[6]/a").text=='6'):
+#               driver.find_element_by_xpath("//*[@id='streams_paginate']/ul/li[6]/a").click()
+#             else:
+#               driver.find_element_by_xpath("//*[@id='streams_paginate']/ul/li[5]/a").click()
+#           if(pagina>2):
+#             if(dataBase==driver.find_element_by_xpath("//*[@id='streams']/tbody/tr[1]/td[1]/a/span").text):
+#               break
+#           for i in range(1,21):
+#             data = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr["+str(i)+"]/td[1]/a/span").text
+#             data = data.replace('"', '')
+#             duracao = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr["+str(i)+"]/td[2]/span").text
+#             viewers = driver.find_element_by_xpath("//*[@id='streams']/tbody/tr["+str(i)+"]/td[3]/span").text
+#             viewers = viewers.replace('"', '')
+#             viewers = viewers.replace(',', '.')
+#             writer.writerow([streamername, data, duracao, viewers, language])
+#             ano= data[data.find(',')+2:data.find(',')+6]
+#           pagina+=1
+#         except:
+#           break
+#     except:
+#       print("Achou nao")
+#       continue  
